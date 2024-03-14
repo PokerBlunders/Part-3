5f9705437e1a6d3fa0afd72a65b2aae5339b5dec
PokerBlunders <ntatti2001@hotmail.com>
2024-03-14 13:23:24 -0400

    Task 4b Complete

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
     public static Villager SelectedVillager { get; private set; }
+    public TextMeshProUGUI type;
+
+    private void Start()
+    {
+        type.text = "No Selection";
+    }
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -14,6 +22,26 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+
+        if (SelectedVillager != null)
+        {
+            CharacterControl.instance.UpdateType(SelectedVillager.GetType().Name);
+        }
+        else
+        {
+            CharacterControl.instance.UpdateType("No Selection");
+        }
     }
-    
+
+    public static CharacterControl instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void UpdateType(string text)
+    {
+        type.text = text;    }
+
 }
