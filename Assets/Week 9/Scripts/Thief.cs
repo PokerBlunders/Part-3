957a62a6f27e879745d5f6dd726adcec5f4bf63f
tattin <ntatti2001@hotmail.com>
2024-03-20 13:58:46 -0400

    Task1b Complete

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -7,15 +7,28 @@ public class Thief : Villager
     public GameObject knifePrefab;
     public Transform spawnPoint1;
     public Transform spawnPoint2;
+    public float dashSpeed = 7;
+
     protected override void Attack()
     {
-        //dash towards mouse
+        StartCoroutine(Dash());
+    }
+
+    IEnumerator Dash()
+    {
         destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         speed = 7;
+        while (speed > 3)
+        {
+            yield return null;
+        }
         base.Attack();
+        yield return new WaitForSeconds(0.1f);
         Instantiate(knifePrefab, spawnPoint1.position, spawnPoint1.rotation);
+        yield return new WaitForSeconds(0.3f);
         Instantiate(knifePrefab, spawnPoint2.position, spawnPoint2.rotation);
     }
+
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
