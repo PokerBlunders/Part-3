ca0e7276cef2e982a911d61d94e143790d3c1851
Nathan Tatti <tattin@shernet.sheridancollege.ca>
2024-03-27 14:36:10 -0400

    Task2 Complete

--- Assets/Week 11/Scripts/Growing.cs
+++ Assets/Week 11/Scripts/Growing.cs
@@ -31,7 +31,7 @@ public class Growing : MonoBehaviour
         StartCoroutine(Square());
         yield return new WaitForSeconds(1);
         coroutine = StartCoroutine(Triangle());
-        Circle();
+        StartCoroutine (Circle());
         yield return coroutine;
         running -= 1;
     }
@@ -64,23 +64,27 @@ public class Growing : MonoBehaviour
         }
         running -= 1;
     }
-    void Circle()
+    IEnumerator Circle()
     {
-
+        running += 1;
         float size = 0;
-        while (size < 5)
-        {
-            size += Time.deltaTime;
-            Vector3 scale = new Vector3(size, size, size);
-            circle.transform.localScale = scale;
-            circleTMP.text = "Cirlce: " + scale;
-        }
-        while (size > 0)
-        {
-            size -= Time.deltaTime;
-            Vector3 scale = new Vector3(size, size, size);
-            circle.transform.localScale = scale;
-            circleTMP.text = "Cirlce: " + scale;
+        while (running >= 1) {
+            while (size < 5)
+            {
+                size += Time.deltaTime;
+                Vector3 scale = new Vector3(size, size, size);
+                circle.transform.localScale = scale;
+                circleTMP.text = "Cirlce: " + scale;
+                yield return null;
+            }
+            while (size > 0)
+            {
+                size -= Time.deltaTime;
+                Vector3 scale = new Vector3(size, size, size);
+                circle.transform.localScale = scale;
+                circleTMP.text = "Cirlce: " + scale;
+                yield return null;
+            }
         }
     }
 }
